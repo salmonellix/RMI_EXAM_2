a046583f22dbae9b139df4b70516023687e352f5
salmonellix <44066048+salmonellix@users.noreply.github.com>
2021-01-19 14:53:59 +0100

    integartion with WS

--- ExamServer/src/constants/Constants.java
+++ ExamServer/src/constants/Constants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class Constants {
 
-    static public int server1Port = 1099;
+    static public int server1Port = 8000;
     static public int maxStudents = 100;
     static public int examTime = 1;
 
